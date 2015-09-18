1fa9a09d8e4ec836941a535ce30c651d5df27644
Eben <eben.lowe@gmail.com>
2015-09-18 14:46:04 -0400

    Working on the Pro Dashboard for MPB.

--- jobs/pro.rb
+++ jobs/pro.rb
@@ -11,15 +11,19 @@ SCHEDULER.every '1m', :first_in => 0 do
   :client_id      => ENV['SALESFORCE_CLIENT_ID'],
   :client_secret  => ENV['SALESFORCE_CLIENT_SECRET']
 
-  pros = client.get '/services/apexrest/DashboardPro', franchisee_id: ENV['FRANCHISEE_ID']
+  pros1 = client.get '/services/apexrest/DashboardPro', franchisee_id: ENV['FRANCHISEE_1_ID']
+  pros2 = client.get '/services/apexrest/DashboardPro', franchisee_id: ENV['FRANCHISEE_2_ID']
+
+  sorted_pros1 = pros1.body.sort_by{|pro| pro.name}
+  sorted_pros2 = pros2.body.sort_by{|pro| pro.name}
+
+  combined_pros = (sorted_pros1 << sorted_pros2).flatten
+
+  combined_sorted_pros = combined_pros.sort_by{|pro| pro.name}
 
   loss = Hash.new
-  loss2 = Hash.new
 
-  group1 = pros.body.sort_by{|pro| pro.name}.each_slice(30).to_a[0]
-  group2 = pros.body.sort_by{|pro| pro.name}.each_slice(30).to_a[1]
-
-  group1.each do |pro|
+  combined_sorted_pros.each do |pro|
     if pro.completed == 0
       loss_percentage = 0
     else
@@ -28,16 +32,6 @@ SCHEDULER.every '1m', :first_in => 0 do
     loss[pro] = { label: pro.name, value: "#{loss_percentage}%"}
   end
 
-  group2.each do |pro|
-    if pro.completed == 0
-      loss_percentage = 0
-    else
-      loss_percentage = (pro.lost/pro.completed.round(2)*100).to_i
-    end
-    loss2[pro] = { label: pro.name, value: "#{loss_percentage}%"}
-  end
-
   send_event('employee_loss', {items: loss.values})
-  send_event('employee_loss_more', {items: loss2.values})
 
 end
